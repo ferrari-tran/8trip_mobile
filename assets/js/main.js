15a13fdf8539400153a9f9e3fe4be84fb4d33867
Thuy Tran Phuong <motdieuuoc.bloom@gmail.com>
2017-10-19 15:33:03 +0700

    working

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ $('.m-btn-switch').click(function(e) {
 
 	var target1 = $(e.currentTarget).data('target-1'),
 			target2 = $(e.currentTarget).data('target-2');
-	
+
 	var value1 = $('[data-ref="' + target1 + '"]').val(),
 			value2 = $('[data-ref="' + target2 + '"]').val();
 
@@ -14,5 +14,22 @@ $('.m-btn-switch').click(function(e) {
 			$('[data-ref="' + target1 + '"]').val(value1);
 			$('[data-ref="' + target2 + '"]').val(value2);
 		}, 10);
+	}
+});
+
+// Double radio
+$(document).ready(function() {
+	var gRadio = $('.m-double-radio');
+	if (gRadio.length > 0) {
+		var bar = $(gRadio).find('.m-double-radio-bar');
+
+		$('[name=round_way]').on('change', function(e) {
+			var label = $(e.currentTarget).parent();
+			if ($(label).index() === 0) {
+				$(bar).css('left', '0');
+			} else {
+				$(bar).css('left', '50%');
+			}
+		});
 	}
 });
